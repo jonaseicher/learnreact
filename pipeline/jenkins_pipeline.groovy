e4efdf92475db3bb90945e7399a7ae6ec5c3a122
jonaseicher <jonaseicher@hotmail.com>
2017-02-03 16:21:23 +0100

    pipeline cleanup

--- pipeline/jenkins_pipeline.groovy
+++ pipeline/jenkins_pipeline.groovy
@@ -41,7 +41,7 @@ node {
 stage "npm publish to nexus"
 node {
     // get hands on the stuff of previous steps
-    unstash 'source'
+    //unstash 'source'
 
     writeFile(file: './.npmrc', text: npmrc, encoding: 'UTF-8')
 
